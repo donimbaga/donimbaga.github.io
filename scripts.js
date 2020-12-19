5f5f36ac97526df318a3811334e4f1f8f77ed9ff
donimbaga <donibrysonimbaga@gmail.com>
2020-12-19 07:58:46 +0300

    Use 'eventListener' on elements

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 
-function myFun() {
+var btn = document.getElementById("btn");
+btn.addEventListener("click", shownhide);
+
+function shownhide() {
   var x = document.getElementById("email");
   x.classList.toggle("hide");
 }

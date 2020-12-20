30ed56c64fe27593b84138e18909cf3f092f6a55
donimbaga <donibrysonimbaga@gmail.com>
2020-12-20 11:28:37 +0300

    click here to copy on clipboad 'done'

--- scripts.js
+++ scripts.js
@@ -5,4 +5,13 @@ btn.addEventListener("click", shownhide);
 function shownhide() {
   var el1 = document.getElementById("btn");
   el1.innerHTML = "donibrysonimbaga@gmail.com";
+
+  var fakeInput = document.createElement("input");
+  fakeInput.value = el1.textContent;
+  document.body.appendChild(fakeInput);
+
+  fakeInput.select();
+  document.execCommand("copy", false);
+
+  fakeInput.remove();
 }

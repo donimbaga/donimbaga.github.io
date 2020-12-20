5e9896d48281963fedff0018a0093b449ae2ad42
donimbaga <donibrysonimbaga@gmail.com>
2020-12-20 10:32:24 +0300

    with just one click

--- scripts.js
+++ scripts.js
@@ -3,6 +3,6 @@ var btn = document.getElementById("btn");
 btn.addEventListener("click", shownhide);
 
 function shownhide() {
-  var x = document.getElementById("email");
-  x.classList.toggle("hide");
+  var el1 = document.getElementById("btn");
+  el1.innerHTML = "donibrysonimbaga@gmail.com";
 }

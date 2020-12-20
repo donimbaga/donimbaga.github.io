073783806404c0889636f87038ff703e16c6a108
donimbaga <donibrysonimbaga@gmail.com>
2020-12-20 17:48:47 +0300

    a b c . . .

--- scripts.js
+++ scripts.js
@@ -3,15 +3,14 @@ var btn = document.getElementById("btn");
 btn.addEventListener("click", shownhide);
 
 function shownhide() {
-  var el1 = document.getElementById("btn");
-  el1.innerHTML = "donibrysonimbaga@gmail.com";
+  var btn = document.getElementById("btn");
+  btn.innerHTML = "donibrysonimbaga@gmail.com";
 
   var fakeInput = document.createElement("input");
-  fakeInput.value = el1.textContent;
+  fakeInput.value = btn.textContent;
   document.body.appendChild(fakeInput);
 
   fakeInput.select();
   document.execCommand("copy", false);
-
   fakeInput.remove();
 }
